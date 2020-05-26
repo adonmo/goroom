537ebf982cf39beeeb8c5ceb5f5e5a220d9de754
Aditya Patwardhan <gamble09D@gmail.com>
2020-05-26 15:58:54 +0530

    Adapter for GORM to use in ROOM

--- room/orm.go
+++ room/orm.go
@@ -1,5 +1,7 @@
 package room
 
+import "github.com/jinzhu/gorm"
+
 //ORM The orm component used by Room
 type ORM interface {
 	HasTable(value interface{}) bool
@@ -7,10 +9,63 @@ type ORM interface {
 	Delete(value interface{}, where ...interface{}) ORM
 	Create(value interface{}) ORM
 	DropTable(values ...interface{}) ORM
-	GetModelStruct(entity interface{}) ModelDefinition
+	GetModelDefinition(entity interface{}) ModelDefinition
 }
 
 //ModelDefinition Interface to access Definition of ORM Entity Model
-type ModelDefinition interface {
-	TableName(ORM) string
+type ModelDefinition struct {
+	TableName   string
+	EntityModel interface{}
+}
+
+//Result Result from DB operations
+type Result struct {
+	Error error
+}
+
+//GORMAdapter Adpater for GORM as used by Room
+type GORMAdapter struct {
+	db *gorm.DB
+}
+
+//HasTable Check Table exists
+func (adapter *GORMAdapter) HasTable(value interface{}) bool {
+	return adapter.db.HasTable(value)
+}
+
+//CreateTable Create a Table
+func (adapter *GORMAdapter) CreateTable(value interface{}) Result {
+	return Result{
+		Error: adapter.db.CreateTable(value).Error,
+	}
+}
+
+//Delete Delete Values
+func (adapter *GORMAdapter) Delete(value interface{}, where ...interface{}) Result {
+	return Result{
+		Error: adapter.db.Delete(value, where).Error,
+	}
+}
+
+//Create Create a row
+func (adapter *GORMAdapter) Create(value interface{}) Result {
+	return Result{
+		Error: adapter.db.Create(value).Error,
+	}
+}
+
+//DropTable Drop a table
+func (adapter *GORMAdapter) DropTable(values ...interface{}) Result {
+	return Result{
+		Error: adapter.db.DropTable(values).Error,
+	}
+}
+
+//GetModelDefinition Get representation of a database table(entity) as done by ORM
+func (adapter *GORMAdapter) GetModelDefinition(entity interface{}) ModelDefinition {
+	model := adapter.db.NewScope(entity).GetModelStruct()
+	return ModelDefinition{
+		EntityModel: model,
+		TableName:   model.TableName(adapter.db),
+	}
 }
