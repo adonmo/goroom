b0b211252b1b5abb81fcdc65e23ebe3ea99e8aec
Aditya Patwardhan <gamble09D@gmail.com>
2020-05-28 17:14:14 +0530

    More upgrade Migration tests

--- room/migration_test.go
+++ room/migration_test.go
@@ -19,7 +19,7 @@ func (suite *MigrationSetupTestSuite) SetupTest() {
 	suite.MockCtrl = gomock.NewController(suite.T())
 
 	upgradeMigrationVersions := [][2]orm.VersionNumber{
-		{2, 3}, {3, 4}, {4, 5}, {2, 5},
+		{2, 3}, {3, 4}, {4, 5}, {2, 5}, {5, 6},
 	}
 
 	downgradeMigrationVersions := [][2]orm.VersionNumber{
@@ -46,8 +46,20 @@ func (suite *MigrationSetupTestSuite) SetupTest() {
 func (suite *MigrationSetupTestSuite) TestGetApplicableMigrationsForUpgrade() {
 
 	migration24, err := GetApplicableMigrations(suite.UpgradeMigrations, 2, 4)
-	isValidUpgradePath := migration24[0].GetBaseVersion() == 2 && migration24[0].GetTargetVersion() == 3 &&
+	isValidUpgradePath := len(migration24) == 2 && migration24[0].GetBaseVersion() == 2 && migration24[0].GetTargetVersion() == 3 &&
 		migration24[1].GetBaseVersion() == 3 && migration24[1].GetTargetVersion() == 4 && err == nil
 	assert.Truef(suite.T(), isValidUpgradePath, "Wrong migration plan %v for 2 to 4 using %v. Err: %v", migration24, suite.UpgradeMigrations, err)
+
+	migration25, err := GetApplicableMigrations(suite.UpgradeMigrations, 2, 5)
+	isValidUpgradePath = len(migration25) == 1 && migration25[0].GetBaseVersion() == 2 && migration25[0].GetTargetVersion() == 5 && err == nil
+	assert.Truef(suite.T(), isValidUpgradePath, "Wrong migration plan %v for 2 to 5 using %v. Err: %v", migration25, suite.UpgradeMigrations, err)
+
+	migration56, err := GetApplicableMigrations(suite.UpgradeMigrations, 5, 6)
+	isValidUpgradePath = len(migration25) == 1 && migration56[0].GetBaseVersion() == 5 && migration56[0].GetTargetVersion() == 6 && err == nil
+	assert.Truef(suite.T(), isValidUpgradePath, "Wrong migration plan %v for 5 to 6 using %v. Err: %v", migration56, suite.UpgradeMigrations, err)
+
+	migration34, err := GetApplicableMigrations(suite.UpgradeMigrations, 3, 4)
+	isValidUpgradePath = len(migration25) == 1 && migration34[0].GetBaseVersion() == 3 && migration34[0].GetTargetVersion() == 4 && err == nil
+	assert.Truef(suite.T(), isValidUpgradePath, "Wrong migration plan %v for 3 to 4 using %v. Err: %v", migration34, suite.UpgradeMigrations, err)
 
 }
